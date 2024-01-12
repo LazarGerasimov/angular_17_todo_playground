f860c7b5048c9c76824a3817948ce0821f72fcf6
Laz <lazgerasimov@gmail.com>
2024-01-12 14:48:03 +0200

    add login form scaffold

--- src/app/shared/layouts/master/master.component.ts
+++ src/app/shared/layouts/master/master.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, HeaderComponent],
   templateUrl: './master.component.html',
   styleUrl: './master.component.scss'
 })
