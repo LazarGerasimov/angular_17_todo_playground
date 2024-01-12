111ff6388393ef0a0560d29b4b21142bdb2f2859
Laz <lazgerasimov@gmail.com>
2024-01-12 14:08:04 +0200

    setup folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { DefaultComponent } from './shared/layouts/default/default.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MasterComponent } from './shared/layouts/master/master.component';
+import { TodoComponent } from './pages/todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    // guest route
+    {
+        path: '',
+        component: DefaultComponent,
+        children: [
+            {
+                path: '', component: LoginComponent
+            }
+        ]
+    },
+    // secured route
+    {
+        path: '',
+        component: MasterComponent,
+        children: [
+            {
+                path: 'todo', component: TodoComponent
+            }
+        ]
+    },
+];
---
+++ src/app/shared/layouts/master/master.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-master',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './master.component.html',
+  styleUrl: './master.component.scss'
+})
+export class MasterComponent {
+
+}
